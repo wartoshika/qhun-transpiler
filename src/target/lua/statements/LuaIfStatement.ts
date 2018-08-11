b8706ca8db5f953fde5b70fccf4e3117f48c060a
wartoshika <dev@qhun.de>
2018-08-11 18:28:41 +0200

    added support for if else and elseif statements

--- src/target/lua/statements/LuaIfStatement.ts
+++ src/target/lua/statements/LuaIfStatement.ts
@@ -14,17 +14,31 @@ export class LuaIfStatement implements Partial<Target> {
         const thenStatement = this.addSpacesToString(this.removeLastNewLine(this.transpileNode(node.thenStatement)), 2);
 
         // check for an else statement
-        let elseStatement: string = "";
+        let elseOrEnd: string = "end";
         if (node.elseStatement) {
 
-            elseStatement = this.transpileNode(node.elseStatement);
+            // look for elseif
+            if (ts.isIfStatement(node.elseStatement)) {
+
+                // add elseif statement
+                elseOrEnd = [
+                    `else${this.transpileNode(node.elseStatement)}`
+                ].join("\n");
+            } else {
+                // add else statement
+                elseOrEnd = [
+                    `else`,
+                    this.addSpacesToString(this.transpileNode(node.elseStatement), 2),
+                    `end`
+                ].join("\n");
+            }
         }
 
         // put all together
         return [
             `if ${condition} then`,
             thenStatement,
-            `end`
+            elseOrEnd
         ].join("\n");
     }
 }
--- test/target/lua/LuaIfStatement.spec.ts
+++ test/target/lua/LuaIfStatement.spec.ts
@@ -18,4 +18,60 @@ import { UnitTest } from "../../UnitTest";
         ]);
 
     }
+
+    @test "If with else statement"() {
+
+        this.runCodeAndExpectResult("lua", [
+            {
+                code: `
+                    if(true) {
+                        print(1);
+                    }else{
+                        print(2);
+                    }
+                `,
+                expected: [
+                    `if true then`,
+                    `  print(1)`,
+                    `else`,
+                    `  print(2)`,
+                    `end`
+                ]
+            }
+        ]);
+    }
+
+    @test "Complex neasted if with elseif statement"() {
+
+        this.runCodeAndExpectResult("lua", [
+            {
+                code: `
+                    if(true) {
+                        if(false) {
+                            print(false);
+                        }else if(true) {
+                            print("3");
+                        }else{
+                            print("yay");
+                        }
+                    }else{
+                        print(2);
+                    }
+                `,
+                expected: [
+                    `if true then`,
+                    `  if false then`,
+                    `    print(false)`,
+                    `  elseif true then`,
+                    `    print("3")`,
+                    `  else`,
+                    `    print("yay")`,
+                    `  end`,
+                    `else`,
+                    `  print(2)`,
+                    `end`
+                ]
+            }
+        ]);
+    }
 }
