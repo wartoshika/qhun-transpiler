ef2927bd5b388af33fed8c850ff967a8b7e319de
wartoshika <dev@qhun.de>
2018-08-21 21:22:41 +0200

    added argument reader validation of given arguments

--- src/config/argument/ArgumentReader.ts
+++ src/config/argument/ArgumentReader.ts
@@ -3,17 +3,36 @@ import { Project } from "../Project";
 import * as path from "path";
 import { ArgumentConfig } from "./ArgumentConfig";
 import { DefaultConfig } from "../DefaultConfig";
+import { Validator } from "../validator/Validator";
+import { ValidatorRule } from "../validator/ValidatorRule";
+import { TargetFactory } from "../../target/TargetFactory";
+import { ValidationError } from "../../error/ValidationError";
 
 export class ArgumentReader implements Reader {
 
     constructor(
-        private args: { [P in keyof ArgumentConfig]?: ArgumentConfig[P] }
+        private args: ArgumentConfig
     ) { }
 
     /**
      * read the given arguments into a project object
      */
     public read(): Project {
+
+        // validate the given argument options
+        const validator = new Validator<ArgumentConfig>({
+            rules: {
+                target: ValidatorRule.isInArray(Object.keys(TargetFactory.supportedTargets)),
+                file: ValidatorRule.pathExists()
+            }
+        });
+
+        // validate the given data and throw an error when the validation fails
+        if (!validator.validate(this.args)) {
+            throw new ValidationError(validator.getValidationErrors());
+        }
+
+        // everything ok, continue
         return DefaultConfig.mergeDefaultProjectData({
             target: this.args.target,
             rootDir: path.dirname(path.resolve(this.args.file)),
--- src/error/ValidationError.ts
+++ src/error/ValidationError.ts
@@ -3,7 +3,7 @@ import { ValidatorErrors } from "../config/validator/Validator";
 export class ValidationError extends Error {
 
     constructor(errors?: ValidatorErrors) {
-        super(`Some errors occured while validating the config file.\n${
+        super(`Some errors occured while validating the given data:\n${
             Object.keys(errors).map(key => {
                 return `${key}: ${errors[key]}`;
             }).join("\n")
